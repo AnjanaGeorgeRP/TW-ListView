6644e0cd5ae19d2c864134ca4c981a8775c0b8c3
AnjanaGeorge <16004097@myrp.edu.sg>
2018-04-23 14:29:33 +0800

    FirstCommit

--- app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/ModuleAdapter.java
@@ -1,10 +1,51 @@
 package com.myapplicationdev.android.tw_listview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ModuleAdapter {
+import java.util.ArrayList;
 
+public class ModuleAdapter extends ArrayAdapter<Module> {
+
+    private ArrayList<Module> module;
     private Context context;
+    private TextView tvModuleCode;
+    private ImageView ivPro;
+
+    public ModuleAdapter(Context context, int resource, ArrayList<Module> objects){
+        super(context, resource, objects);
+        module = objects;
+        this.context = context;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.row, parent, false);
+
+        tvModuleCode = (TextView) rowView.findViewById(R.id.tvModuleCode);
+        // Get the ImageView object
+        ivPro = (ImageView) rowView.findViewById(R.id.ivPro);
+
+        Module currentModule = module.get(position);
+        // Set the TextView to show the food
+
+        tvModuleCode.setText(currentModule.getName());
+        // Set the image to star or nostar accordingly
+        if(currentModule.isProgramming()) {
+            ivPro.setImageResource(R.drawable.prog);
+        }
+        else {
+            ivPro.setImageResource(R.drawable.nonprog);
+        }
+        return rowView;
+    }
+
 
 
 
--- app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/SecondActivity.java
@@ -3,13 +3,18 @@ package com.myapplicationdev.android.tw_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     TextView tvYear;
+    ArrayAdapter aa;
+    ArrayList<Module> module;
 
 
     @Override
@@ -20,11 +25,27 @@ public class SecondActivity extends AppCompatActivity {
         lv = (ListView) this.findViewById(R.id.lvModules);
         tvYear = (TextView) findViewById(R.id.tvYear);
 
+        module = new ArrayList<Module>();
+        aa = new ModuleAdapter(this, R.layout.row, module);
+        lv.setAdapter(aa);
+
+
         Intent i = getIntent();
         String year = i.getStringExtra("year");
         tvYear.setText(year);
-
-
+        if(year == "Year 1"){
+            module.add(new Module("C105", true));
+            module.add(new Module("B102", false));
+            module.add(new Module("A113", false));
+        }if(year == "Year 1"){
+            module.add(new Module("C208", false));
+            module.add(new Module("C200", true));
+            module.add(new Module("C346", false));
+        }else{
+            module.add(new Module("C347", true));
+            module.add(new Module("C302", true));
+            module.add(new Module("C349", true));
+        }
 
     }
 
